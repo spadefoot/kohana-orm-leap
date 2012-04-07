32fc6f443ddffa3eb4f7c2661dc039b58dd765b4
Matthew Mattson <matt.mattson@ifrogz.com>
2012-04-07 13:25:35 -0600

    Adding auth schema files for PostgreSQL and SQLite.

--- schema/auth/mysql.sql
+++ schema/auth/mysql.sql
@@ -26,7 +26,7 @@
 ----
 
 ----
--- Table structure for the `roles` table
+-- Table structure for the "roles" table
 ----
 
 CREATE TABLE IF NOT EXISTS `roles` (
@@ -38,14 +38,14 @@ CREATE TABLE IF NOT EXISTS `roles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ----
--- Roles for the `roles` table 
+-- Roles for the "roles" table 
 ----
 
--- INSERT INTO `roles` (`id`, `name`, `description`) VALUES(1, 'login', 'Login privileges, granted after account confirmation');
--- INSERT INTO `roles` (`id`, `name`, `description`) VALUES(2, 'admin', 'Administrative user, has access to everything.');
+-- INSERT INTO `roles` (`id`, `name`, `description`) VALUES (1, 'login', 'Login privileges, granted after account confirmation.');
+-- INSERT INTO `roles` (`id`, `name`, `description`) VALUES (2, 'admin', 'Administrative user, has access to everything.');
 
 ----
--- Table structure for the `users` table
+-- Table structure for the "users" table
 ----
 
 CREATE TABLE IF NOT EXISTS `users` (
@@ -55,14 +55,14 @@ CREATE TABLE IF NOT EXISTS `users` (
 	`password` varchar(64) NOT NULL,
 	`firstname` varchar(35) DEFAULT NULL,
 	`lastname` varchar(50) DEFAULT NULL,
-	`activated` tinyint(1) NOT NULL DEFAULT '1',
-	`banned` tinyint(1) NOT NULL DEFAULT '0',
+	`activated` tinyint(1) NOT NULL DEFAULT 1,
+	`banned` tinyint(1) NOT NULL DEFAULT 0,
 	`ban_reason` varchar(255) DEFAULT NULL,
 	`new_password_key` varchar(64) DEFAULT NULL,
 	`new_password_requested` int(11) DEFAULT NULL,
 	`new_email` varchar(254) DEFAULT NULL,
 	`new_email_key` varchar(64) DEFAULT NULL,
-	`logins` int(10) UNSIGNED NOT NULL DEFAULT '0',
+	`logins` int(10) UNSIGNED NOT NULL DEFAULT 0,
 	`last_login` int(10) UNSIGNED DEFAULT NULL,
 	`last_ip` varchar(39) DEFAULT NULL,
 	PRIMARY KEY (`id`),
@@ -71,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ----
--- Table structure for the `user_roles` table
+-- Table structure for the "user_roles" table
 ----
 
 CREATE TABLE IF NOT EXISTS `user_roles` (
@@ -79,10 +79,10 @@ CREATE TABLE IF NOT EXISTS `user_roles` (
 	`role_id` int(10) UNSIGNED NOT NULL,
 	PRIMARY KEY (`user_id`,`role_id`),
 	KEY `fk_role_id` (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ----
--- Table structure for the `user_tokens` table
+-- Table structure for the "user_tokens" table
 ----
 
 CREATE TABLE IF NOT EXISTS `user_tokens` (
@@ -99,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `user_tokens` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ----
--- Constraints for the `user_roles` table
+-- Constraints for the "user_roles" table
 ----
 
 ALTER TABLE `user_roles`
@@ -107,7 +107,7 @@ ALTER TABLE `user_roles`
 	ADD CONSTRAINT `user_roles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
 
 ----
--- Constraints for the `user_tokens` table
+-- Constraints for the "user_tokens" table
 ----
 
 ALTER TABLE `user_tokens`
