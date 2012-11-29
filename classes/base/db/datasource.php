355e3c1cbd59a1eecb2efe7e3b47da735118a0e5
Matthew A Mattson <matt.mattson@ifrogz.com>
2012-11-29 11:18:46 -0700

    Merged code changed posted on Issue #74.

--- classes/base/db/datasource.php
+++ classes/base/db/datasource.php
@@ -21,7 +21,7 @@
  *
  * @package Leap
  * @category Connection
- * @version 2012-08-16
+ * @version 2012-11-29
  *
  * @abstract
  */
@@ -91,6 +91,7 @@ abstract class Base_DB_DataSource extends Kohana_Object {
 			case 'port':
 			case 'type':
 			case 'username':
+			case 'role':
 				return $this->settings[$name];
 			break;
 			default:
@@ -168,6 +169,10 @@ abstract class Base_DB_DataSource extends Kohana_Object {
 
 		$this->settings['username'] = (isset($settings['connection']['username']))
 			? (string) $settings['connection']['username']
+			: '';
+
+		$this->settings['role'] = (isset($settings['connection']['role']))
+			? (string) $settings['connection']['role']
 			: '';
 	}
 
